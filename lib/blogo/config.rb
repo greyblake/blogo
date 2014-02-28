8aea6bb77c6e6e29d9a806a290353abf1c876bd8
Potapov Sergey <blake131313@gmail.com>
2014-02-28 20:19:20 +0200

    Add social icons

--- lib/blogo/config.rb
+++ lib/blogo/config.rb
@@ -2,13 +2,20 @@ module Blogo
   class Config
     attr_accessor :site_title
     attr_accessor :site_subtitle
+
     attr_accessor :table_name_prefix
+
     attr_accessor :posts_per_page
     attr_accessor :paginator_size
     attr_accessor :recent_posts
-    attr_accessor :disqus_shortname
-    attr_accessor :google_analytics_id
+
     attr_accessor :keywords
+
+    attr_accessor :disqus_shortname
+    attr_accessor :twitter_username
+    attr_accessor :github_username
+    attr_accessor :google_analytics_id
+
     attr_accessor :use_ckeditor
 
     attr_reader :markup_lang
