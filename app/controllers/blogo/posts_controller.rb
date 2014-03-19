89ee7fd655c2c55577d6579afffccb988fef84b8
Potapov Sergey <blake131313@gmail.com>
2014-03-19 15:24:17 +0200

    Add doc to PostsController

--- app/controllers/blogo/posts_controller.rb
+++ app/controllers/blogo/posts_controller.rb
@@ -1,38 +1,45 @@
 module Blogo
+  # Responsible for showing posts and atom feeds to visitors.
   class PostsController < ApplicationController
     layout 'blogo/blog'
 
     # Number of posts shown in feed.
     FEED_POSTS_LIMIT = 20
 
+    # GET /posts
+    #
     def index
       @tag = params[:tag]
       set_vars
       set_paginator
 
       @meta = {}
-      @meta[:title]         = "#{Blogo.config.site_title} - #{Blogo.config.site_subtitle}"
-      @meta[:site_name]     = Blogo.config.site_title
-      @meta[:keywords]      = Blogo.config.keywords
-      @meta[:type]          = 'website'
+      @meta[:title]     = "#{Blogo.config.site_title} - #{Blogo.config.site_subtitle}"
+      @meta[:site_name] = Blogo.config.site_title
+      @meta[:keywords]  = Blogo.config.keywords
+      @meta[:type]      = 'website'
     end
 
+    # GET /posts/:permalink
+    #
     def show
       @post = Post.published.where(:permalink => params[:permalink]).first!
       set_vars
 
       @meta = {}
-      @meta[:title]         = "#{@post.title} - #{Blogo.config.site_title}"
-      @meta[:description]   = @post.meta_description
-      @meta[:keywords]      = [@post.tags_string, Blogo.config.keywords].flatten.join(", ")
-      @meta[:url]           = request.url
-      @meta[:image]         = meta_image
-      @meta[:type]          = 'article'
-      @meta[:site_name]     = Blogo.config.site_title
+      @meta[:title]       = "#{@post.title} - #{Blogo.config.site_title}"
+      @meta[:description] = @post.meta_description
+      @meta[:keywords]    = [@post.tags_string, Blogo.config.keywords].flatten.join(", ")
+      @meta[:url]         = request.url
+      @meta[:image]       = meta_image
+      @meta[:type]        = 'article'
+      @meta[:site_name]   = Blogo.config.site_title
     end
 
+    # GET /posts/feed
+    #
     def feed
-      @posts   = published_posts.limit(FEED_POSTS_LIMIT)
+      @posts   = Post.published.limit(FEED_POSTS_LIMIT)
       @updated = @posts.first.try(:updated_at)
 
       render 'feed', layout: false
@@ -41,25 +48,28 @@ module Blogo
 
     private
 
+    # Build a paginator object and set to @paginator.
+    #
+    # @return [Blogo::Paginator]
     def set_paginator
-      posts = published_posts
-      posts = posts.joins(:tags).where("#{Tag.table_name}.name = ?", @tag) if @tag
+      posts_scope = Post.published
+
+      if @tag
+        posts_scope = posts_scope.joins(:tags).where("#{Tag.table_name}.name = ?", @tag)
+      end
 
       @paginator = Paginator.new(
-        posts,
+        posts_scope,
         :page     => (params[:page] || 1),
         :per_page => conf.posts_per_page,
         :size     => conf.paginator_size)
     end
 
+    # Sets @recent_posts and @tags.
+    #
     def set_vars
-      @recent_posts = published_posts.limit(conf.recent_posts) if conf.recent_posts
-      @tags = Tag.all
-    end
-
-
-    def published_posts
-      Post.published
+      @recent_posts = Post.published.limit(conf.recent_posts) if conf.recent_posts
+      @tags         = Tag.all
     end
 
     # Engine configuration. Method +config+ is already used by Rails.
@@ -69,9 +79,10 @@ module Blogo
       Blogo.config
     end
 
-
+    # @return [String, nil] full URL to image or nil
     def meta_image
       return nil unless @post.meta_image.present?
+
       if @post.meta_image =~ /\Ahttp/
         @post.meta_image
       else
